e25be56a03e23d591d08bafc181457f3b09a5208
captivea-pgi <pierre-louis.giacosa@captivea.com>
2021-09-13 14:00:38 +0200

    adding fields to the table

--- estate/models/estate_properties.py
+++ estate/models/estate_properties.py
@@ -7,3 +7,23 @@ class RealEstate(models.Model):
     _description = "Real estate management"
 
     name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Type',
+        selection=[
+            ('north','North'),
+            ('south','South'),
+            ('east','East'),
+            ('west','West')
+        ]
+    )
