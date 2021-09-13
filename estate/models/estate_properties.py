5ea98408f2c87f44b51b35d2e0684bc42f8291f2
captivea-pgi <pierre-louis.giacosa@captivea.com>
2021-09-13 17:37:56 +0200

    fix

--- estate/models/estate_properties.py
+++ estate/models/estate_properties.py
@@ -9,7 +9,7 @@ class RealEstate(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.today()+relativedelta(month=+6))
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.today())
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default="2")
