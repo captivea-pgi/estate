43d6926ba69aa8204be1b5483105dada06b74cb2
captivea-pgi <pierre-louis.giacosa@captivea.com>
2021-09-13 10:18:59 +0200

    new manifest

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'CRM',
+    'version': '0.1',
+    'author': "Pierre-Louis Giacosa",
+    'description': """
+        This is a test module for real estate management
+        """,
+    'depends': [
+        'base',
+    ]
+}
